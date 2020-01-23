03445563a002e5855a1e11c6ae2169c7b1e55786
Michael <skaman4459@gmail.com>
2020-01-22 19:24:57 -0500

    mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -20,10 +20,10 @@ function findById(id) {
 }
 
 function findSteps(id) {
-    return db('schemes')
-        .join('steps', 'schemes.id', 'steps.scheme_id')
-        .where('schemes.id', id)
-        .select('scheme.id', 'steps.scheme_name', 'steps.step_number', 'step.instructions')
+    return db('steps')
+        .join('schemes', 'schemes.id', 'steps.scheme_id')
+        .select('schemes.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+        .where('steps.scheme_id', id)
 } 
 
 function add(scheme) {
